0ce7d2a0e4446c011ee82722139d27cfcead9b23
Mamadou Seydou DIALLO <diallodiery301@gmail.com>
2025-09-21 18:40:04 +0200

    Observability and database config

---
+++ web/src/main/java/com/samasouf/controller/ControllerTest.java
@@ -0,0 +1,25 @@
+package com.samasouf.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import com.samasouf.service.MyUserService;
+
+@RestController
+class MyController {
+
+    private static final Logger log = LoggerFactory.getLogger(MyController.class);
+    private final MyUserService myUserService;
+
+    MyController(MyUserService myUserService) {
+        this.myUserService = myUserService;
+    }
+
+    @GetMapping("/user/{userId}")
+    String userName(@PathVariable("userId") String userId) {
+        log.info("Got a request");
+        return myUserService.userName(userId);
+    }
+}
